edccbd36826b2d805e772da7151000c416b56099
William Ku <kucheech@gmail.com>
2022-02-04 11:31:42 +0800

    add keyboard * button will output letter in console on click and set background color

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+const row1Letters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+const row2Letters = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+const row3Letters = ['ENTER','Z','X','C','V','B','N','M', '<x]']
+
+const addButton = ($row, letter) => {
+    const $button = $('<button>')
+    $button.text(letter)
+    $row.append($button)
+}
+
+const addKeyboard = () => {
+    const $row1 = $('#row1')
+    row1Letters.forEach(ch => addButton($row1, ch))
+    const $row2 = $('#row2')
+    row2Letters.forEach(ch => addButton($row2, ch))
+    const $row3 = $('#row3')
+    row3Letters.forEach(ch => addButton($row3, ch))
+}
+
 $(() => {
 
+    addKeyboard()
+
+    const buttons = $('button')
+    buttons.click((e) => {
+        const letter = e.currentTarget.innerText
+        console.log('clicked', letter)
+        // e.currentTarget.disabled = true
+        e.currentTarget.style.backgroundColor = 'darkgrey'
+    })
 });
